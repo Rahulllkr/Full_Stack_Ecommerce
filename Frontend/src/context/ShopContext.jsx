6136353c0d20648ba5081a354dbc7049fa32f87c
Rahulllkr <rahulkumar8604745@gmail.com>
2025-11-12 02:13:48 +0530

    about us page updated

--- Frontend/src/context/ShopContext.jsx
+++ Frontend/src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useState } from "react";
 import { products } from "../assets/assets";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 export const ShopContext = createContext();
 
@@ -11,6 +12,7 @@ const ShopContextProvider = (props) => {
     const [search,setSearch] = useState("");
     const [showSearch,setShowSearch] = useState(false)
     const [cartItems,setCartItems] = useState({})
+    const navigate = useNavigate()
 
     const addToCart = async(itemId,size) => {
 
@@ -59,7 +61,7 @@ const ShopContextProvider = (props) => {
         setCartItems(cartData)
     }
 
-    const getCartAmount = async() => {
+    const getCartAmount = () => {
         let totalAmount = 0;
         for(const items in cartItems){
             let itemInfo = products.find((product) => product._id === items)
@@ -80,7 +82,7 @@ const ShopContextProvider = (props) => {
         products,currency,delivery_fee,
         search,setSearch,showSearch,setShowSearch,
         cartItems,addToCart,getCartCount,updateQuantity,
-        getCartAmount
+        getCartAmount,navigate
     }
 
 
