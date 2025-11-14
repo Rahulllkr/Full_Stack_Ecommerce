d3f7f29b5a3d5d820aab10705ae3a61f8ab56bf8
Rahulllkr <rahulkumar8604745@gmail.com>
2025-11-14 17:25:22 +0530

    user auth complete

--- Backend/server.js
+++ Backend/server.js
@@ -1,24 +1,32 @@
 import express from "express"
 import cors from "cors"
-import 'dotenv/config'
+import "dotenv/config"
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+import userRouter from "./routes/userRoute.js";
+import productRouter from "./routes/productRoute.js";
+
 
 
 //App Config
 
 const app = express();
 const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
 
 //Middlewares
 app.use(express.json());
 app.use(cors());
 
 //Api endpoints
+app.use("/api/user",userRouter)
+app.use("/api/product",productRouter)
 
 app.get("/", (req,res) => {
     res.send("Server setup successfully")
 });
 
-//extra route remove.
 
 app.listen(port,()=>{
     console.log("Server is running on Port : "+ port)
