fdf237b7f4e1362967852774ecfd60300dbf3500
Rahulllkr <rahulkumar8604745@gmail.com>
2025-11-27 13:15:44 +0530

    buffer updated

--- Backend/config/mongodb.js
+++ Backend/config/mongodb.js
@@ -5,7 +5,10 @@ const connectDB = async () => {
     mongoose.connection.on("connected",() => {
         console.log("DB Connected")
     })
-    await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`,{
+        bufferTimeoutMs:30000, //30 seconds
+        serverSelectionTimeoutMS:50000  // 50 seconds
+    })
 }
 
 export default connectDB
